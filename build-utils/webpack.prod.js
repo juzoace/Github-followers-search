3ace2231e88067b7772cc062745dc412efe1bcb8
Nwigwe Uzochukwu <uzochukwunwigwe@gmail.com>
2020-12-13 09:56:59 +0100

    Added some changes to the webpack.prod.js file and webpack.dev.js file....Modified the folder structure

--- build-utils/webpack.prod.js
+++ build-utils/webpack.prod.js
@@ -1,35 +1,4 @@
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-// module.exports = {
-//     entry: "./src/index.js",
-// devtool: 'source-map',
-//     mode: "production",
-//     output: {
-//       path: path.join(__dirname, '/dist'),
-//       filename: "index_bundle.js"  
-//     },
-//     module: {
-//         rules: [
-//             {
-//                test: /\.js$/, 
-//                exclude: /node_modules/,
-//                use: {
-//                    loader: "babel-loader"
-//                }
-//             }
-//         ]
-//     },
-//     plugins: [
-//         new HtmlWebpackPlugin({
-//             template: "./src/index.html"
-//         })
-//     ]
-// }
-
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path')
 
 module.exports = {
     entry: path.resolve(__dirname, './src/index.js'),
@@ -37,28 +6,44 @@ module.exports = {
     module: {
       rules: [
         {
-          test: /\.(js)$/,
+          test: /\.(js|jsx)$/,
           exclude: /node_modules/,
-          use: ['babel-loader'],
+          use: [
+            {
+            loader: "babel-loader"
+          }
+      ],
         },
+        {
+          test: /\.(sass|css|less)$/,
+          use: [
+              {
+                  loader: 'style-loader'
+              },
+              {
+                  loader: 'css-loader'
+              },
+              {
+                  loader: 'less-loader',
+                  options: {
+                      lessOptions: {
+                          javascriptEnabled: true,
+                      },
+                  }
+              }
+          ]
+      }
       ],
     },
     resolve: {
       extensions: ['*', '.js'],
+      alias: {
+            'react': path.resolve(__dirname, './node_modules/react'),
+            'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+        }
     },
     output: {
       path: path.resolve(__dirname, './dist'),
       filename: 'bundle.js',
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-        title: 'Hello Webpack bundled JavaScript Project',
-        template: path.resolve(__dirname, './src/index.html'),
-        }
-        
-    )],
-    devServer: {
-      contentBase: path.resolve(__dirname, './dist'),
     },
   };
